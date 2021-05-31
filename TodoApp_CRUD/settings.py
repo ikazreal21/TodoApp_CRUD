f26cfe028f40c7103a88ab0300f3a2a4bc72d755
ikazreal21 <expliot21@gmail.com>
2021-05-31 22:33:23 +0800

    staticfiles

--- TodoApp_CRUD/settings.py
+++ TodoApp_CRUD/settings.py
@@ -122,6 +122,8 @@ LOGIN_URL = "login"
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
+STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles")
+
 STATIC_URL = "/static/"
 
 MEDIA_URL = "/img/"
